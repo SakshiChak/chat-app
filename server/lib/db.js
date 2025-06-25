532780875fe1d88ba1372c837fbef11dbf13eeae
SakshiChak <mailme.sakshichak@gmail.com>
2025-06-25 17:54:30 +0530

    Chat App Project

---
+++ server/lib/db.js
@@ -0,0 +1,11 @@
+import mongoose from "mongoose";
+
+// Function to connect to MongoDB Database
+export const connectDB = async () => {
+    try {
+        mongoose.connection.on('connected', () => console.log('Database Connected'));
+        await mongoose.connect(`${process.env.MONGODB_URI}/chat-app`);
+    } catch (error) {
+        console.error("Error connecting to MongoDB:", error);
+    }
+};
